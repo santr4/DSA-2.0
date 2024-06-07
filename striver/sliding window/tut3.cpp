76635a82da0e9da5a4e220cc4a259d86fbad54ab
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-06-08 00:45:49 +0530

    [solved] : 1248. Count Number of Nice Subarrays

---
+++ striver/sliding window/tut3.cpp
@@ -0,0 +1,30 @@
+// 1248. Count Number of Nice Subarrays
+
+class Solution
+{
+public:
+    int f(vector<int> &nums, int goal)
+    {
+        if (goal < 0)
+        {
+            return 0;
+        }
+        int l = 0, r = 0, sum = 0, cnt = 0;
+        while (r < nums.size())
+        {
+            sum += (nums[r] % 2);
+            while (sum > goal)
+            {
+                sum -= (nums[l] % 2);
+                l++;
+            }
+            cnt += (r - l + 1);
+            r++;
+        }
+        return cnt;
+    }
+    int numberOfSubarrays(vector<int> &nums, int k)
+    {
+        return f(nums, k) - f(nums, k - 1);
+    }
+};
