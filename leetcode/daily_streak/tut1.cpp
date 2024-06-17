360ef9e6f98b04553a1187a6db931dde32c5658a
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-06-17 14:25:08 +0530

    [solved] : 633. Sum of Square Numbers - Approach 2

--- leetcode/daily_streak/tut1.cpp
+++ leetcode/daily_streak/tut1.cpp
@@ -18,3 +18,33 @@ public:
         return false;
     }
 };
+
+// ** Approach 2
+
+class Solution
+{
+public:
+    bool judgeSquareSum(int c)
+    {
+        long left = 0;
+        long right = static_cast<long>(sqrt(c));
+
+        while (left <= right)
+        {
+            long curr_sum = left * left + right * right;
+            if (curr_sum == c)
+            {
+                return true;
+            }
+            if (curr_sum < c)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        return false;
+    }
+};
