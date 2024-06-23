ea2cc15dec8f8b7dc9a2386d95d033bdcd80d031
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-06-23 11:01:21 +0530

    [solved] : 33. Search in Rotated Array

---
+++ striver/binary search/tut5.cpp
@@ -0,0 +1,44 @@
+// 33. Search in Rotated Sorted Array
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            // now we will check which half is sorted -> left or right.
+            if (nums[low] <= nums[mid])
+            {
+                if (nums[low] <= target && target <= nums[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            if (nums[mid] <= nums[high])
+            {
+                if (nums[mid] <= target && target <= nums[high])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
