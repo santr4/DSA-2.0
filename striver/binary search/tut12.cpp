00da56b90c0afd7e87ec52e947452aab760afe92
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-07-01 22:58:08 +0530

    [solved] : 410. Split Array Largest Sum

---
+++ striver/binary search/tut12.cpp
@@ -0,0 +1,45 @@
+// 410. Split Array Largest Sum
+
+class Solution
+{
+public:
+    int countPartitions(vector<int> &nums, int maxSum)
+    {
+        int n = nums.size();
+        int partitions = 1;
+        long long subarraySum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (subarraySum + nums[i] <= maxSum)
+            {
+                subarraySum += nums[i];
+            }
+            else
+            {
+                partitions++;
+                subarraySum = nums[i];
+            }
+        }
+        return partitions;
+    }
+
+    int splitArray(vector<int> &nums, int k)
+    {
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(), 0);
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            int partitions = countPartitions(nums, mid);
+            if (partitions > k)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
