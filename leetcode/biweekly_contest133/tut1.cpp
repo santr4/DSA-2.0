658f6c90da581d5a8b6b30377dc0fec4bb40337a
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-07-18 20:39:00 +0530

    3190. Find min operations to make all elements divisible by three

---
+++ leetcode/biweekly_contest133/tut1.cpp
@@ -0,0 +1,19 @@
+// 3190. Find Minimum Operations to Make All Elements Divisible by Three
+
+class Solution
+{
+public:
+    int minimumOperations(vector<int> &nums)
+    {
+        int n = nums.size();
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] % 3 != 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+};
