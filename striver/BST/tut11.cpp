3c2bf81d8929dccfdfd9610614538db2f5b34b9c
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-07-19 14:41:19 +0530

    1008. Construct a BST from preorder traversal

---
+++ striver/BST/tut11.cpp
@@ -0,0 +1,34 @@
+// 1008. Construct Binary Search Tree from Preorder Traversal
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    TreeNode *bstFromPreorder(vector<int> &preorder)
+    {
+        int i = 0;
+        return f(preorder, i, INT_MAX);
+    }
+    TreeNode *f(vector<int> &preorder, int &i, int bound)
+    {
+        if (i == preorder.size() || preorder[i] > bound)
+        {
+            return nullptr;
+        }
+        TreeNode *root = new TreeNode(preorder[i]);
+        i++;
+        root->left = f(preorder, i, root->val);
+        root->right = f(preorder, i, bound);
+        return root;
+    }
+};
