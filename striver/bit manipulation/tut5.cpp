3ace67a0807d137f0a4834c48a7c01f38fc03f0f
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-07-24 18:32:37 +0530

    set the rightmost unset bit

---
+++ striver/bit manipulation/tut5.cpp
@@ -0,0 +1,22 @@
+// set the rightmost unset bit.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int setRightmostUnsetBit(int n)
+{
+    // if all bits of 'n' are set
+    // the number is of form 2^k -1 return n
+    if (!(n & (n + 1)))
+        return n;
+    // else
+    return n | (n + 1);
+}
+
+// Driver program to test above
+int main()
+{
+    int n = 21;
+    cout << setRightmostUnsetBit(n);
+    return 0;
+}
