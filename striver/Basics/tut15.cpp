e585c876c67bc51e39e9a647d491c5455e1621b6
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-07-29 16:47:01 +0530

    print 1 to N without loop

---
+++ striver/Basics/tut15.cpp
@@ -0,0 +1,17 @@
+// Print 1 To N Without Loop
+
+class Solution
+{
+public:
+    // Complete this function
+    void printNos(int N)
+    {
+        // Your code here
+        if (N == 0)
+        {
+            return;
+        }
+        printNos(N - 1);
+        cout << N << " ";
+    }
+};
