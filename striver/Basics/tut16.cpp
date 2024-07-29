c86dc18238857b1e385867e014ab07a5b6023ab9
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-07-29 16:53:20 +0530

    print GFG n times

---
+++ striver/Basics/tut16.cpp
@@ -0,0 +1,16 @@
+// Print GFG n times
+
+class Solution
+{
+public:
+    void printGfg(int N)
+    {
+        // Code here
+        if (N == 0)
+        {
+            return;
+        }
+        printGfg(N - 1);
+        cout << "GFG" << " ";
+    }
+};
