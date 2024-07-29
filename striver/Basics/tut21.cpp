f9557c2b3f6c1fe55dd01818a5655efdd952f934
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-07-29 18:30:21 +0530

    frequencies of limited range array elements

---
+++ striver/Basics/tut21.cpp
@@ -0,0 +1,27 @@
+// Frequencies of Limited Range Array Elements
+
+class Solution
+{
+public:
+    // Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int> &arr, int N, int P)
+    {
+        // code here
+        unordered_map<int, int> mp;
+        for (int i = 0; i < N; i++)
+        {
+            mp[arr[i]]++;
+        }
+        for (int i = 0; i < N; i++)
+        {
+            if (mp.find(i + 1) != mp.end())
+            {
+                arr[i] = mp[i + 1];
+            }
+            else
+            {
+                arr[i] = 0;
+            }
+        }
+    }
+};
