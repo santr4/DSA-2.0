fede9089e706db86fc07030b5841de26936c69ca
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-07-31 01:35:28 +0530

    optimized solution for longest subarray sum K using two pointers

--- striver/arrays/tut18.cpp
+++ striver/arrays/tut18.cpp
@@ -29,3 +29,33 @@ public:
         return maxLen;
     }
 };
+
+// the below solution is only for arrays with zeroes and positive integers.
+
+// class Solution{
+//     public:
+//     int lenOfLongSubarr(int A[],  int N, int K)
+//     {
+//         // Complete the function
+//         int left = 0;
+//         int right = 0;
+//         int sum = A[0];
+//         int maxLen = 0;
+//         while(right < N){
+//             // to reduce the window size when sum exceeds K
+//             while(left <= right && sum > K){
+//                 sum -= A[left];
+//                 left++;
+//             }
+//             if(sum == K){
+//                 maxLen = max(maxLen, right - left + 1);
+//             }
+//             right++;
+//             if(right < N){
+//                 sum += A[right];
+//             }
+//         }
+//         return maxLen;
+//     }
+
+// };
